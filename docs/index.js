ed6c6c5fddb4f166860f45ed979f5bab4a20e43b
Mridul <mehariamridul@gmail.com>
2018-12-17 17:52:41 +0530

    Fixed all tooltip issues

--- docs/index.js
+++ docs/index.js
@@ -1,20 +1,97 @@
-const cumulativeCalculation = (dm, variable, cumulativeVariableName) => {
+const cumulativeCalculationAirlineDate = dm => {
+  let currDate = "";
+  let currAirLine = "";
   let i = 0;
-  return dm.calculateVariable(
+  let j = 0;
+  // Create a variable to create pseudo axis
+  return dm.sort([["Airline"], ["Monthly Date"]]).calculateVariable(
     {
-      name: cumulativeVariableName,
+      name: "Number of Incidents by Airline",
       type: "measure",
-      defAggFn: "sum"
+      defAggFn: "first"
     },
     [
-      variable,
-      inc => {
-        i = i + inc;
-        return i;
+      "Airline",
+      "Monthly Date",
+      (al, date) => {
+        if (date === currDate && al === currAirLine) {
+          j++;
+          return i;
+        } else if (al !== currAirLine) {
+          i = 1;
+          j = 0;
+          currDate = date;
+          currAirLine = al;
+          return 1;
+        } else {
+          currDate = date;
+          i = i + j + 1;
+          j = 0;
+          return i;
+        }
       }
     ]
   );
 };
+
+const calcCumulative = (dm, variable, cumulativeVariableName) => {
+  let currDate = "";
+  let i = 0;
+  let j = 0;
+  // Create a variable to create pseudo axis
+  return dm.calculateVariable(
+    {
+      name: cumulativeVariableName,
+      type: "measure",
+      defAggFn: "first"
+    },
+    [
+      variable,
+      date => {
+        if (date === currDate) {
+          j++;
+          return i;
+        } else {
+          currDate = date;
+          i = i + j + 1;
+          j = 0;
+          return i;
+        }
+      }
+    ]
+  );
+};
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+
+const tooltipFormatter = (dataModel, variables)=>{
+    const tooltipData = dataModel.getData().data;
+    const fieldConfig = dataModel.getFieldsConfig();
+
+    let tooltipContent = [];
+    tooltipData.forEach((dataArray, i) => {
+        const monthlyDate = dataArray[fieldConfig[variables[0]].index];
+        const num = dataArray[fieldConfig[variables[1]].index];
+        const date =  new Date(monthlyDate)
+
+        tooltipContent[0] = [{
+            value: variables[0],
+            className: 'muze-tooltip-key'
+        }, {
+            value: `${months[date.getMonth()]}-${date.getFullYear()}`,
+            className: 'muze-tooltip-value'
+        }]
+        
+        tooltipContent[1]= [{
+            value:variables[1],
+            className: 'muze-tooltip-key'
+        }, {
+            value: num,
+            className: 'muze-tooltip-value'
+        }]
+    });
+    return tooltipContent
+}
 
 function jsUcfirst(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
@@ -49,18 +126,28 @@ fetch("data.json").then(resp =>
       }
     ];
 
-    const dm = new DataModel(nData, schema);
-    let sdm = dm.sort([["Date"]]);
+    let dm = new DataModel(nData, schema);
 
-    // Create a variable to create pseudo axis
-    sdm = sdm.calculateVariable(
-      {
-        name: "incident-count",
-        type: "measure",
-        defAggFn: "sum"
-      },
-      ["Date", () => 1]
-    );
+    // Create a variable to create monthly data
+    dm = dm.calculateVariable(
+        {
+          name: "Monthly Date",
+          type: "dimension",
+          subtype: "temporal",
+          format: "%m-%Y"
+        },
+        [
+          "Date",
+          d => {
+            const date = new Date(d);
+            const month =
+              date.getMonth() < 10 ? `0${date.getMonth()}` : date.getMonth();
+            return `${month}-${date.getFullYear()}`;
+          }
+        ]
+      );
+  
+    let sdm = dm.sort([["Date"]]);
 
     // Create a variable to create year data
     sdm = sdm.calculateVariable(
@@ -76,10 +163,31 @@ fetch("data.json").then(resp =>
       ]
     );
 
+    // Create a variable to create pseudo axis
+    sdm = sdm.calculateVariable(
+      {
+        name: "incident-count",
+        type: "measure",
+        defAggFn: "sum"
+      },
+      ["Date", () => 1]
+    );
+
+    // DataModel for the cumulative line chart
+   let incidentDm = calcCumulative(
+      sdm,
+      "Monthly Date",
+      "Number of Incidents"
+    );
+
     let airlineDM = sdm.groupBy(["Airline"], {
       "incident-count": "count"
     });
+
+    // Getting list of airlines
     const airlines = airlineDM.getData().data;
+
+    // DataModel for the heatmap
     airlineDM = airlineDM.calculateVariable(
       {
         name: "incident-count-2",
@@ -93,34 +201,15 @@ fetch("data.json").then(resp =>
       ]
     );
 
-    // Create a variable to create year data
-    let incidentDm = sdm.calculateVariable(
-      {
-        name: "Monthly Date",
-        type: "dimension",
-        subtype: "temporal",
-        format: "%m-%Y"
-      },
-      [
-        "Date",
-        d => {
-          const date = new Date(d);
-          const month =
-            date.getMonth() < 10 ? `0${date.getMonth()}` : date.getMonth();
-          return `${month}-${date.getFullYear()}`;
-        }
-      ]
-    );
-    incidentDm = incidentDm.groupBy(["Monthly Date"]);
-    incidentDm = cumulativeCalculation(
-      incidentDm,
-      "incident-count",
-      "incident-count-cumulative"
-    );
+    // DataModel for the trellis
+    const trellisDM = cumulativeCalculationAirlineDate(dm);
+
+    // Main KPI
     document.getElementById(
       "number-of-incidents-content"
     ).innerHTML = sdm.groupBy([""]).getData().data[0][0];
 
+    // Canvas for incident counts
     const canvas1 = muze()
       .canvas()
       .data(airlineDM.sort([["incident-count"]]))
@@ -188,10 +277,11 @@ fetch("data.json").then(resp =>
       })
       .mount("#incidents-breakup");
 
+    // Canvas for cumulative incident counts
     const canvas2 = muze()
       .canvas()
       .data(incidentDm)
-      .rows(["incident-count-cumulative"])
+      .rows(["Number of Incidents"])
       .columns(["Monthly Date"])
       .color({
         value: "#414141"
@@ -207,10 +297,16 @@ fetch("data.json").then(resp =>
           y: {
             name: "Number of Incidents"
           }
+        },
+        interaction: {
+          tooltip: {
+            formatter: dataModel => tooltipFormatter(dataModel, ['Monthly Date',  "Number of Incidents"])
+          }
         }
       })
       .mount("#incidents-by-year-step-content");
 
+    // Canvas for the heat map airline vs year
     const canvas3 = muze()
       .canvas()
       .data(sdm)
@@ -234,7 +330,6 @@ fetch("data.json").then(resp =>
       .mount("#incidents-by-year-heatmap-content");
 
     const trellisCanvases = [];
-
     airlines.forEach((e, i) => {
       const parentDiv = document.getElementById(
         "incidents-by-year-trellis-content"
@@ -249,17 +344,13 @@ fetch("data.json").then(resp =>
       for (let j = 2009; j <= 2019; j++) {
         dates.push(new Date(j, 0, 1));
       }
-      const newDm = cumulativeCalculation(
-        sdm.select(fields => fields.Airline.value === e[0]).groupBy(["Date"]),
-        "incident-count",
-        "incident-count-cumulative"
-      );
+      let newDm = trellisDM.select(fields => fields.Airline.value === e[0]);
 
       const canvas = muze()
         .canvas()
         .data(newDm)
-        .rows(["incident-count-cumulative"])
-        .columns(["Date"])
+        .rows(["Number of Incidents by Airline"])
+        .columns(["Monthly Date"])
         .color({
           value: "#414141"
         })
@@ -273,7 +364,13 @@ fetch("data.json").then(resp =>
             y: {
               name: "Number of Incidents"
             }
+          },
+          interaction: {
+            tooltip: {
+              formatter: dataModel => tooltipFormatter(dataModel, ['Monthly Date',  "Number of Incidents by Airline"])
+            }
           }
+
         })
         .mount(newElement);
 
